aa9bdc99eb0d32c1c1902aafc380283f66bf2104
AnastasiaV11 <89535376+AnastasiaV11@users.noreply.github.com>
2022-01-30 17:06:25 +0200

    Update config.py

--- config.py
+++ config.py
@@ -30,10 +30,10 @@ API_ID = int(getenv("API_ID", ""))
 API_HASH = getenv("API_HASH", "")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
 SESSION_STRING = getenv("SESSION_STRING", "")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "AsmSupport")
-UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "AsmSafone")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "RomaniaDeep")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "Vamos")
 ASSISTANT_NAME = getenv("ASSISTANT_NAME", "MyVideoPlayer")
-SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "ID").split()))
 REPLY_MESSAGE = getenv("REPLY_MESSAGE", "")
 if not REPLY_MESSAGE:
     REPLY_MESSAGE = None
